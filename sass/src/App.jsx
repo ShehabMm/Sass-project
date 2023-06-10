ffdd30cd019911027a45351ca9836d579307840f
shehabMm <shehab.mm@gmail.com>
2023-06-10 15:11:45 +0200

    first commit

---
+++ sass/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import './style.css'
+function App() {
+
+  return (
+    <>
+    <header>
+    <p>shehab</p>
+    <button>submit</button>
+
+
+    </header>
+  <div className="info">
+<h1>Our contact info</h1>
+<p>This is our info</p>
+  </div>
+    </>
+  )
+}
+
+export default App
