7f1fb74c9ff91408cf2b8dfb7b9bc5100e7d42ef
shehabMm <shehab.mm@gmail.com>
2023-06-14 11:08:55 +0200

    api

--- sass/src/pages/popup/popup.jsx
+++ sass/src/pages/popup/popup.jsx
@@ -1,25 +1,79 @@
 import './popup.css';
 import CloseIcon from '@mui/icons-material/Close';
 import PropTypes from 'prop-types'
+import { useState } from 'react';
 
-Popup.propTypes = {
 
-data: PropTypes.func
 
-}
+const Popup = ({ data }) => {
+  
+  Popup.propTypes = {
 
-const Popup = ({data}) => {
+    data: PropTypes.func
+
+  }
+
+  const [array, setarray] = useState([]);
+  const [subtask, setsubtask] = useState("");
+  const [title, settitle] = useState("");
+
+
+  const addFun = () => {
+
+    array.push(subtask)
+    setsubtask("")
+  }
+
+
+
+
+
+
+
   return (
     <>
       <form >
 
-<CloseIcon className='close'  onClick={() => {
+        <CloseIcon className='close' onClick={() => {
 
-data()
-          }} />
-    
-      <input type="text" placeholder="write" />
-      <button>submit</button>
+          data()
+        }} />
+
+        <input
+          onChange={(eo) => {
+            settitle(eo.target.value)
+          }}
+          type="text" placeholder="title" />
+        <input
+
+          onChange={(eo) => {
+            setsubtask(eo.target.value)
+            console.log(subtask)
+          }}
+
+
+
+          type="text" placeholder="details" value={subtask} />
+
+        <button
+          onClick={(eo) => {
+
+            eo.preventDefault()
+            addFun()
+
+          }}>Add</button>
+        {array.map((item) => {
+
+          return (
+
+            <ul key={item}><li>
+              {item}
+            </li></ul>
+
+
+          )
+
+        })}
       </form>    </>
   );
 }
